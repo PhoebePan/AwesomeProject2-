c1ec85faee0b2ceec4d23296840ee89dea18f15c
Phoebe Pan <phoebe.pan@yuelaobank.com>
2017-11-02 17:29:06 +0800

    feat: create tabbar

--- App.js
+++ App.js
@@ -9,28 +9,37 @@ import {
   Button
 } from "react-native";
 import { Router, Scene, Tabs } from "react-native-router-flux";
+import "babel-polyfill";
 
 import LoginScreen from "./screens/LoginScreen";
 import MeScreen from "./screens/MeScreen";
 import ChatListScreen from "./screens/ChatListScreen";
 import ChatScreen from "./screens/ChatScreen";
 
-const App = () => {
-  return (
-    <Router>
-      <Scene key="root">
-        <Scene key="login" component={LoginScreen} title="Login" initial />
-        <Scene tabs={true} key="me" component={MeScreen} title="About Me" />
-        <Scene
-          tabs={true}
-          key="chatlist"
-          component={ChatListScreen}
-          title="Chat List"
-        />
-        <Scene key="chat" component={ChatScreen} title="Chat" />
-      </Scene>
-    </Router>
-  );
-};
-
-export default App;
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Scene key="root">
+          <Scene key="login" component={LoginScreen} title="Login" initial />
+          {/* Tab Container */}
+          <Scene
+            key="tabbar"
+            tabs={true}
+            tabBarStyle={{ backgroundColor: "#FFFFFF" }}
+            hideNavBar
+          >
+            <Scene
+              tabs={true}
+              key="chatlist"
+              component={ChatListScreen}
+              title="Chat List"
+            />
+            <Scene key="chat" component={ChatScreen} title="Chat" />
+            <Scene tabs={true} key="me" component={MeScreen} title="About Me" />
+          </Scene>
+        </Scene>
+      </Router>
+    );
+  }
+}
