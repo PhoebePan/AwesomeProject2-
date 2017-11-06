cf217ddd08fec1b8d02de3f92809af6e39ad5503
Phoebe Pan <phoebe.pan@yuelaobank.com>
2017-11-06 18:33:46 +0800

    feat: add verify lightbox

--- App.js
+++ App.js
@@ -8,37 +8,63 @@ import {
   TouchableOpacity,
   Button
 } from "react-native";
-import { Router, Scene, Tabs } from "react-native-router-flux";
+import { Router, Scene, Tabs, Lightbox } from "react-native-router-flux";
 import "babel-polyfill";
 
 import LoginScreen from "./screens/LoginScreen";
 import MeScreen from "./screens/MeScreen";
 import ChatListScreen from "./screens/ChatListScreen";
 import ChatScreen from "./screens/ChatScreen";
+import VerifyLightbox from "./components/lightbox/VerifyLightbox";
+
+const TabIcon = ({ selected, title }) => {
+  return <Text style={{ color: selected ? "red" : "black" }}>{title}</Text>;
+};
 
 export default class App extends React.Component {
   render() {
     return (
       <Router>
-        <Scene key="root">
-          <Scene key="login" component={LoginScreen} title="Login" initial />
-          {/* Tab Container */}
-          <Scene
-            key="tabbar"
-            tabs={true}
-            tabBarStyle={{ backgroundColor: "#FFFFFF" }}
-            hideNavBar
-          >
+        <Lightbox>
+          <Scene key="root">
             <Scene
-              tabs={true}
-              key="chatlist"
-              component={ChatListScreen}
-              title="Chat List"
+              key="login"
+              component={LoginScreen}
+              title="Login"
+              hideNavBar
+              initial
             />
-            <Scene key="chat" component={ChatScreen} title="Chat" />
-            <Scene tabs={true} key="me" component={MeScreen} title="About Me" />
+            {/* Tab Container */}
+            <Scene
+              key="tabbar"
+              tabs={true}
+              tabBarStyle={{ backgroundColor: "#79006E" }}
+              hideNavBar
+              tabBarPosition={"bottom"}
+            >
+              <Scene
+                key="chatlist"
+                component={ChatListScreen}
+                title="Chat List"
+                icon={TabIcon}
+              />
+              <Scene
+                key="chat"
+                component={ChatScreen}
+                title="Chat"
+                icon={TabIcon}
+              />
+              <Scene
+                key="me"
+                component={MeScreen}
+                title="About Me"
+                icon={TabIcon}
+              />
+            </Scene>
           </Scene>
-        </Scene>
+          {/* Verify Lightbox */}
+          <Scene key="verify_lightbox" component={VerifyLightbox} hideNavBar />
+        </Lightbox>
       </Router>
     );
   }
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -7,7 +7,7 @@ export default class LoginScreen extends Component {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Login Screen</Text>
-        <Button title="Login" onPress={() => Actions.me()} />
+        <Button title="Verify Box" onPress={Actions.verify_lightbox} />
       </View>
     );
   }
